eac134d062dad73fcf2329d025af887ec39b4baf
suhettgabriel <suhettgabriel1983@gmail.com>
2025-08-06 09:51:12 -0300

    feat(application): create dtos for genre
    - Add GenreDto for data transfer.
    - Add CreateGenreDto for creating new genres, with validation.
    - Add UpdateGenreDto for updating existing genres, with validation.

---
+++ Biblioteca/Biblioteca.Application/DTOs/GenreDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Application.DTOs.Genre
+{
+    public class GenreDto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+    }
+}
---
+++ Biblioteca/Biblioteca.Application/DTOs/UpdateGenreDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Application.DTOs.Genre
+{
+    public class UpdateGenreDto
+    {
+        [Required(ErrorMessage = "O nome do gênero é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome do gênero deve ter no máximo 100 caracteres.")]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+    }
+}
