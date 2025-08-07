6e14c47796b930491d0e34befc84e0a5dafd4828
suhettgabriel <suhettgabriel1983@gmail.com>
2025-08-07 09:03:25 -0300

    feat(frontend): create main application layout

--- Biblioteca-App/src/app/app-module.ts
+++ Biblioteca-App/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './core/components/header/header';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header
   ],
   imports: [
     BrowserModule,
