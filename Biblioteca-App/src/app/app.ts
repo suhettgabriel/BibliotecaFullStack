29210432d72575c98aac9c3738ab7acf5c50d641
suhettgabriel <suhettgabriel1983@gmail.com>
2025-08-07 08:33:55 -0300

    feat(frontend): initialize stable NgModule-based angular project

--- Biblioteca-App/src/app/app.ts
+++ Biblioteca-App/src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
+  standalone: false,
   styleUrl: './app.scss'
 })
 export class App {
--- Biblioteca-App/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://api-de-producao.com/api'
-};
